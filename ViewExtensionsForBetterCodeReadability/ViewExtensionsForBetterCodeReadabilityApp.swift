9a9ddce1024a744bd07b501fa49fdc26bf3e8252
Rick Brown <ricky.brown.00@gmail.com>
2021-10-06 12:30:49 +0100

    Initial Commit

---
+++ ViewExtensionsForBetterCodeReadability/ViewExtensionsForBetterCodeReadabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewExtensionsForBetterCodeReadabilityApp.swift
+//  ViewExtensionsForBetterCodeReadability
+//
+//  Created by Rick Brown on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewExtensionsForBetterCodeReadabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
